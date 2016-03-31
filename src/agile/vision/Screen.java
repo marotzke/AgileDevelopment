dde4c3e5340fd1e3a5d4e907852ffba86acf21fa
matheusdmd <matheusdmd@al.insper.edu.br>
2016-03-31 09:48:38 -0300

    Drawing AND Gate

--- src/agile/vision/Screen.java
+++ src/agile/vision/Screen.java
@@ -1,6 +1,8 @@
 package agile.vision;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class Screen extends JPanel{
@@ -13,9 +15,25 @@ public class Screen extends JPanel{
 	private int height;
 	
 	public Screen(){
-		this.width  = 2000;
-		this.height = 2000;
+		this.width  = 500;
+		this.height = 500;
 		
 		setPreferredSize(new Dimension(this.width, this.height));
+	}
+	
+	public void paintComponent(Graphics g) {
+		drawAndGate(g);
+	}
+	
+	public void drawAndGate(Graphics g) {
+		int size  = 15;
+		
+		g.drawLine(size*5, size*5, size*10, size*5);
+		g.drawLine(size*5, size*10, size*10, size*10);
+		g.drawLine(size*10, size*2, size*10, size*12);
+		g.drawArc(size*6, size*2, size*10, size*10, -100, 200);
+		g.drawLine(size*16,size*7,size*22,size*7);
+		
+		getToolkit().sync();
 	}
 }

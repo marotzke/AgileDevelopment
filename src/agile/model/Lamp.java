bb6ea99b5a8ad24a48e24bee8cffd3b9095a327a
matheusdmd <matheusdmd@al.insper.edu.br>
2016-04-04 16:07:11 -0300

    try Lamp

--- src/agile/model/Lamp.java
+++ src/agile/model/Lamp.java
@@ -1,11 +1,24 @@
 package agile.model;
 
 public class Lamp implements Drain{
-
+	
+	private InputPin pin;
+	
+	public Lamp(int index){
+		pin = new InputPin(index);
+	}
+	
 	@Override
 	public InputPin getInputPin(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return pin;
+	}
+	
+	public void setPinLampSource(Source source){
+		pin.setSource(source);
+	}
+	
+	public boolean isOn(){
+		return pin.getSource().getOutputValue(pin.getIndex());
 	}
 
 }

e7b19b9b24513b54eba31bcf415b9b79415d95de
matheusdmd <matheusdmd@al.insper.edu.br>
2016-04-05 07:45:50 -0300

    Bug fixed

--- src/agile/vision/XorGateDrawer.java
+++ src/agile/vision/XorGateDrawer.java
@@ -28,8 +28,7 @@ public class XorGateDrawer extends GateDrawer{
 			g.drawArc(locationX+size*7, locationY+size*2, size*3, size*11, -100, 200);
 			g.drawArc(locationX+size*3, locationY+size*2, size*13, size*11, -100, 200);
 			g.drawArc(locationX+size*6, locationY+size*2, size*3, size*11, -100, 200);
-			g.drawLine(locationX+size*18,locationY+size*7,locationX+size*22,locationY+size*7);
-			g.drawArc(locationX+size*16, locationY+size*6, size*2, size*2, -20, 360);
+			g.drawLine(locationX+size*16,locationY+size*7,locationX+size*22,locationY+size*7);
 			
 			this.drawGround(g);
 			this.drawGroundCarry(g);
